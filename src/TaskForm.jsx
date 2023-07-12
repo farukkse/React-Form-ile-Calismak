ae68ead0bb4720eafa6f5c7d80b074b30a8458a3
farukkse <mr.foruk@hotmail.com>
2023-07-12 18:40:30 +0300

    react form

---
+++ src/TaskForm.jsx
@@ -0,0 +1,145 @@
+import React, { useEffect, useState } from 'react'
+import TaskList from './TaskList'
+import { v4 as uuidv4 } from 'uuid'
+
+function TaskForm() {
+
+  const emptyForm = {
+    task: '',
+    priority: false,
+    isDone: false
+  }
+  const [formData, setFormData] = useState(emptyForm)
+  const [tasks, setTasks] = useState([])
+  const [taskChangeCount, setTaskChangeCount] = useState(0)
+
+  const defaultFalse = false;
+  // sayfa ilk acildiginda islem yap
+  useEffect(() => {
+    const localStorageTasks = JSON.parse(localStorage.getItem('tasks'))
+    setTasks(localStorageTasks ?? [])
+  }, [])
+
+  // tasks bilgisi degisince islem yap
+
+  useEffect(() => {
+    if (taskChangeCount > 0) {
+      localStorage.setItem('tasks', JSON.stringify(tasks))
+    }
+  }, [taskChangeCount])
+
+  function doneTask(uuid) {
+
+    const taskIndex = tasks.findIndex(item => item.uuid === uuid)
+    const task = tasks[taskIndex]
+    task.isDone = !task.isDone;
+
+    const newTasks = tasks.slice()
+    newTasks[taskIndex] = task
+    setTasks(newTasks)
+    setTaskChangeCount(prev => prev + 1)
+  }
+  function editTask(uuid) {
+    console.log(uuid)
+
+    const task = tasks.find(item => item.uuid === uuid)
+    console.log(task)
+    setFormData({ ...task, isEdited: true })
+    setTaskChangeCount(prev => prev + 1)
+
+  }
+  function removeTask(uuid) {
+
+    console.log(uuid)
+
+    setTasks(prev => prev.filter(item => item.uuid !== uuid))
+    setTaskChangeCount(prev => prev + 1)
+  }
+
+  function handleInputChange(event) {
+    setFormData(prev => {
+      return {
+        ...prev,
+        [event.target.name]: event.target.type === 'checkbox' ? event.target.checked : event.target.value
+      }
+    })
+
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    console.log(formData)
+    if (formData.isEdited) {
+      const taskIndex = tasks.findIndex(item => item.uuid === formData.uuid)
+      const newTasks = tasks.slice()
+      newTasks[taskIndex] = { ...formData }
+      setTasks(newTasks)
+
+
+    }
+    else if (formData.task.length > 3) {
+
+      formData.uuid = uuidv4()
+
+      setTasks(prev => [formData, ...prev])
+
+    }
+
+    setTaskChangeCount(prev => prev + 1)
+    setFormData(emptyForm)
+    event.target.reset()
+
+  }
+
+  return (
+    <>
+
+      <TaskList
+        tasks={tasks}
+        removeTask={removeTask}
+        editTask={editTask}
+        doneTask={doneTask}
+        defaultFalse={defaultFalse} />
+
+
+      <form onSubmit={handleFormSubmit}>
+        <div className="row mb-3">
+          <label htmlFor="task" className="col-sm-2 col-form-label">Task</label>
+          <div className="col-sm-10">
+            <input
+              type="text"
+              className="form-control"
+              id="task"
+              name='task'
+              value={formData.task}
+              onChange={handleInputChange}
+
+            />
+          </div>
+        </div>
+
+
+        <div className="row mb-3">
+          <div className="col-sm-10 offset-sm-2">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                id="priority"
+                name='priority'
+                checked={formData.priority}
+                onChange={handleInputChange} />
+
+              <label className="form-check-label" htmlFor="priority">
+                Oncelikli
+              </label>
+            </div>
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary">Kaydet</button>
+      </form>
+    </>
+  )
+}
+
+export default TaskForm
---
+++ src/TaskList.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import TaskListItem from "./TaskListItem";
+
+function TaskList({ tasks, removeTask, editTask, doneTask, defaultFalse }) {
+
+  const [priority, setPriority] = useState(false)
+  const [filteredTasks, setFilteredTask] = useState(tasks)
+
+  function handlePriorityFilter() {
+    setPriority(prev => !prev)
+  }
+
+  // useEffect(() => {// func. calisacak} //[eger array bos ise ilk component yuklenince anlamina gelir])
+  // task bilgisi componente ulasinca filtera esitle
+  useEffect(() => {
+    setFilteredTask(tasks)
+    setPriority(defaultFalse)
+
+  }, [tasks])
+
+  // priority bilgisi degisirse
+  useEffect(() => {
+    priority ? setFilteredTask(tasks.filter(item => item.priority === priority)) : setFilteredTask(tasks)
+  }, [tasks, priority])
+
+  if (tasks.length === 0) {
+    return <></>
+  }
+
+  return (
+    <>
+      <div className="p-4 bg-light mb-5 border rounded">
+        <h4 className="mb-3">Gorevler:
+          <button onClick={handlePriorityFilter}
+            className={`btn btn-sm ${!priority ? "btn-info" : "btn-primary"} float-end`}>
+            {
+              !priority ? 'Oncelikli Olanlari Goster' : 'Hepsini Goster ...'}</button>
+        </h4>
+        <ul className="list-group">
+          {filteredTasks.map(
+            (task) => (
+              <TaskListItem key={task.uuid} task={task} editTask={editTask} removeTask={removeTask} doneTask={doneTask} />
+            ))}
+        </ul>
+      </div >
+    </>
+  );
+}
+
+export default TaskList;
